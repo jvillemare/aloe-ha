402ca6f2bc88b8ea39cf1a496d81b252d3e44125
Viva Laas <vivalaas@udel.edu>
2020-04-18 13:21:29 -0400

    Added unit testing for PlotDesign

---
+++ GardenProject/src/test/udel/windows/PlotDesignTest.java
@@ -0,0 +1,44 @@
+package test.udel.windows;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.udel.plants.Plant;
+import main.udel.windows.PlotDesign;
+
+public class PlotDesignTest {
+
+	@Test
+	public void PlotDesignTest() {
+		PlotDesign p = new PlotDesign();
+		System.out.println(p.getTitle());
+		assertTrue(p.getTitle().equals("Plot Designer"));
+	}
+	
+	@Test
+	public void getObstacleTest() {
+		PlotDesign p = new PlotDesign();
+		p.getObstacle();
+	}
+
+	@Test
+	public void setObstacleTest() {
+		PlotDesign p = new PlotDesign();
+		p.setObstacle();
+	}
+	
+	@Test
+	public void getPlantTest() {
+		PlotDesign p = new PlotDesign();
+		p.getPlant();
+	}
+	
+	@Test
+	public void setPlantTest() {
+		PlotDesign p = new PlotDesign();
+		p.setPlant(new Plant(null, null, null, 0, null, null));
+		
+	}
+	
+}
