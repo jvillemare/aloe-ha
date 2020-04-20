b1b1291f540ceb46d22a2fd177dd07a5c121abe6
Viva Laas <vivalaas@udel.edu>
2020-04-20 11:17:48 -0400

    Added documentation to SoilTypes to showcase where information about soil type and ph level was found

--- GardenProject/src/main/udel/enums/SoilTypes.java
+++ GardenProject/src/main/udel/enums/SoilTypes.java
@@ -1,8 +1,22 @@
 package main.udel.enums;
 
 /**
- * Borrowed from 
+ * SoilTypes from 
  * https://learn.eartheasy.com/articles/know-your-garden-soil-how-to-make-the-most-of-your-soil-type/
+ * 
+ * Acidity Levels from:
+ * https://en.wikipedia.org/wiki/Soil_pH
+ * https://greenharvest.com.au/GreenGardenNotes/UnderstandingSoilpH.html
+ * https://learn.eartheasy.com/articles/know-your-garden-soil-how-to-make-the-most-of-your-soil-type/
+ * https://www.rhs.org.uk/advice/profile?pid=762
+ * 
+ * Created PH ranges: 
+ *   - acidic = lower PH 
+ *   - alkaline = higher PH
+ *   - adaptable = PH level can be adjusted by adding certain materials
+ * 
+ * Process follows this website's plant soil PH:
+ * https://mtcubacenter.org/plants/moonbeam-whorled-tickseed/
  *
  * @author Team 0
  */
