2b0988b0e889d5bbc1949b17519114dfb5e04dd6
Megha Patel <mpatel@udel.edu>
2020-04-23 22:19:47 -0400

    Existing plants: features added to screen

--- GardenProject/src/main/java/udel/GardenProject/windows/ExistingPlants.java
+++ GardenProject/src/main/java/udel/GardenProject/windows/ExistingPlants.java
@@ -3,9 +3,16 @@ package udel.GardenProject.windows;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+
+import javax.swing.event.ChangeListener;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -15,6 +22,8 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -37,14 +46,28 @@ public class ExistingPlants extends Window {
 	// TODO: Change to ArrayList?
 	private Plant existingPlant[];
 
-	private Button backToMain;
-	private Button nextButton;
+	private Button backToMain, save, nextButton;
 
+	/**
+	 * Used for the search box and the options that show up 
+	 */
 	private GridPane container;
 	private HBox searchBox;
 
+	/**
+	 * Used for top message 
+	 */
 	private Text text1;
 
+	private TilePane tilePane;
+	
+	private ScrollPane scroll;
+	
+	/**
+	 * Used for displaying what the user selected 
+	 */
+	private static VBox selection;
+	
 	/**
 	 * Create an ExistingPlants window instance.
 	 *
@@ -53,10 +76,10 @@ public class ExistingPlants extends Window {
 	public ExistingPlants(Model m) {
 		super(m, "What Plants are in your Garden?");
 
-
-
 		container = new GridPane();
 		searchBox = new HBox();
+		
+		tilePane = new TilePane();
 
 		// this is just for testing
 		String[] options = {"pine tree",
@@ -66,18 +89,25 @@ public class ExistingPlants extends Window {
 		TextField text = new TextField();
 
 		text.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(container.getChildren().size()>1){ // if already contains a drop-down menu -> remove it
+            if(container.getChildren().size()>1){ 
                 container.getChildren().remove(1);
             }
-            container.add(populateDropDownMenu(newValue, options),0,1); // then add the populated drop-down menu to the second row in the grid pane
+            container.add(populateDropDownMenu(newValue, options),0,1);
         });
+		
         Button close = new Button("X");
-        Button search = new Button("Search");
-        searchBox.getChildren().addAll(text,close,search);
+        close.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	System.out.println("X: removing typing");
+            	text.setText("");
+            }
+        });
+        
+        searchBox.getChildren().addAll(text,close);
 
         container.add(searchBox, 0, 0);
-        container.setBackground(new Background(new BackgroundFill(Color.GRAY, null,null)));
-
+        container.setBackground(new Background(new BackgroundFill(Color.GREEN, null,null)));
 
 		borderPane = new BorderPane();
 		vbox = new VBox();
@@ -85,37 +115,61 @@ public class ExistingPlants extends Window {
 		text1 = new Text("Which plants are already in your Garden?");
 		text1.setWrappingWidth(800);
 		text1.setStyle("-fx-font-size: 20px;");
+		
 		vbox.getChildren().addAll(text1);
+		vbox.setPadding(new Insets(10, 10, 10, 10));
 
 
 		backToMain = new Button("Back to Main");
 		backToMain.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent event) {
+            	System.out.println("Back to Main: going back to main");
             	switchToWindow(Windows.Welcome);
+            }
+        });
+		
+		save = new Button("Save");
+		save.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	System.out.println("Save: saving existing plants");
+            	// get existing plants function
             }
         });
 
 		nextButton = new Button("Next");
 		nextButton.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent event) {
+            	System.out.println("Next: going to questionnaire");
             	switchToWindow(Windows.Questionnaire);
             }
         });
 
-
-
+		tilePane.setAlignment(Pos.CENTER);
+		tilePane.setPadding(new Insets(5));
+		tilePane.setHgap(100);
+		tilePane.getChildren().addAll(backToMain, save, nextButton);
+		
+		scroll = new ScrollPane();
+		scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+		scroll.setVmax(440);
+        scroll.setPrefSize(690, 600);
+        scroll.setContent(vbox);
+		
+        selection = new VBox();
+        
+        vbox.getChildren().add(selection);
+        
+		borderPane.setRight(scroll);
+		borderPane.setLeft(container);
 		borderPane.setTop(vbox);
-		borderPane.setCenter(container);
-		borderPane.setBottom(backToMain);
-		borderPane.setRight(nextButton);
+		borderPane.setBottom(tilePane);
 
 		this.root = new Group();
 		root.getChildren().add(borderPane);
-		this.scene = new Scene(this.root);
+		this.scene = new Scene(this.root, 860, 640);
 	}
 
 	// TODO: Constructor to pass in plant array?
@@ -145,34 +199,46 @@ public class ExistingPlants extends Window {
 
 	}
 
-
-
 	 public static VBox populateDropDownMenu(String text, String[] options){
-	        VBox dropDownMenu = new VBox();
-	        dropDownMenu.setBackground(new Background(new BackgroundFill(Color.YELLOW, null,null))); // colors just for example
-	        dropDownMenu.setAlignment(Pos.CENTER); // all these are optional and up to you
-
-	        for(String option : options){ // loop through every String in the array
-	            // if the given text is not empty and doesn't consists of spaces only, as well as it's a part of one (or more) of the options
-	            if(!text.replace(" ", "").isEmpty() && option.toUpperCase().contains(text.toUpperCase())){
-	                Label label = new Label(option); // create a label and set the text
-	                // you can add listener to the label here if you want
-	                // your user to be able to click on the options in the drop-down menu
-	                dropDownMenu.getChildren().add(label); // add the label to the VBox
-	            }
-	        }
-
-	        return dropDownMenu; // at the end return the VBox (i.e. drop-down menu)
-	    }
-
-
-
-
-
-
-
-
-
-
+		 VBox dropDownMenu = new VBox();
+		 dropDownMenu.setBackground(new Background(new BackgroundFill(Color.DARKSEAGREEN, null,null))); 
+		 dropDownMenu.setAlignment(Pos.CENTER);
+		 
+		 for(String option : options){ 
+			 if(!text.replace(" ", "").isEmpty() && option.toUpperCase().contains(text.toUpperCase())){
+				 Label label = new Label(option); 
+	                
+				 label.setOnMouseClicked(new EventHandler<MouseEvent>() {
+					 @Override
+					 public void handle(MouseEvent mouseEvent) {
+						 if(mouseEvent.getButton().equals(MouseButton.PRIMARY)){
+							 if(mouseEvent.getClickCount() == 1){
+								 label.setStyle("-fx-font-weight: bold");
+								 TextArea textarea = new TextArea(label.getText());
+								 textarea.setPrefHeight(label.getHeight() + 10);
+								 System.out.println("You selected a plant from the dropdown --> " + label.getText());
+								 
+								 Button deleteButton = new Button("X"); 
+								 HBox selectedPlant = new HBox();
+								 selectedPlant.setMaxWidth(Double.MAX_VALUE);
+								 selectedPlant.getChildren().addAll(textarea, deleteButton);
+								 
+								 selection.getChildren().addAll(selectedPlant);
+								 deleteButton.setOnAction(new EventHandler<ActionEvent>() {
+									 @Override
+									 public void handle(ActionEvent event) {
+										 System.out.println("X: removing selection");
+										 textarea.clear();
+	                                    	}
+	                                	});
+	                            	}
+	                        	}
+	                    	}
+	                	});
+	                	dropDownMenu.getChildren().add(label);
+			 }
+	        	}
+	        	return dropDownMenu;
+	    	}
 
 }
