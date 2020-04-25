64d8216f7da2af272b5ebf3ee4a7171c50fc33e9
Megha Patel <mpatel@udel.edu>
2020-04-24 23:04:11 -0400

    Significant additions to PlotDesign screen

--- GardenProject/src/main/java/udel/GardenProject/windows/PlotDesign.java
+++ GardenProject/src/main/java/udel/GardenProject/windows/PlotDesign.java
@@ -2,11 +2,23 @@ package udel.GardenProject.windows;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import udel.GardenProject.enums.Windows;
 import udel.GardenProject.garden.Model;
@@ -23,15 +35,73 @@ public class PlotDesign extends Window {
 	private Group root;
 	private Scene scene;
 
+	/**
+	 * Used for the overall layout
+	 */
 	private BorderPane borderPane;
-	private VBox vbox;
+
+	/**
+	 * Used to hold text, left and right panels
+	 */
+	private VBox vbox, leftDropdownVBox, autoRateVBox;
+
 	private Text text;
 
+	/**
+	 * Used for labeling autorate bars
+	 */
+	private Text animalsFedTxt, contBloomTxt, matchTxt, transitionTxt;
+
+	/**
+	 * Text above editPlotButton
+	 */
+	private Text editPlotText;
+
+	/**
+	 * Used inside of leftDropdownVBox for the plants we will put in the plot
+	 */
+	private TilePane tilePane;
+
 	private int statistics[];
-	private Button saveButton;
-	private Button backButton;
-	private Button loadButton;
-	private Button nextButton;
+
+	/**
+	 * Buttons in tilePane at the bottom of the screen
+	 */
+	private Button backButton, saveButton, loadButton, nextButton;
+
+	/**
+	 * Used for when the user wants to edit the points of their plot
+	 */
+	private Button editPlotButton;
+
+	/**
+	 * Holds the options for what the user wants to put in their plot
+	 */
+	private HBox optionHBox;
+
+	/**
+	 * Used for toggling options to display
+	 */
+	private ToggleGroup itemGroup;
+	private ToggleButton existingPlants, selectedPlants, obstacles;
+
+	private FlowPane flow;
+
+	/**
+	 * Used for scrolling in the flowPane for all the options the user can all to
+	 * their plot
+	 */
+	private ScrollPane scroll;
+
+	/**
+	 * Bars showing status of each autorate
+	 */
+	private Rectangle animalsFedBar, contBloomBar, matchBar, transitionBar;
+
+	/**
+	 * Used to outline the center area
+	 */
+	private Rectangle box;
 
 	/**
 	 * Create a new PlotDesign window instance.
@@ -43,38 +113,196 @@ public class PlotDesign extends Window {
 
 		borderPane = new BorderPane();
 		vbox = new VBox();
+		leftDropdownVBox = new VBox();
+		leftDropdownVBox.setBackground(new Background(new BackgroundFill(Color.SEASHELL, null, null)));
+		tilePane = new TilePane();
 
-		text = new Text("Welcome to the Plot Design!\n");
+		text = new Text(
+				"Welcome to the Plot Design! Place all of your plants and objects on your plot to complete your garden!");
 		text.setWrappingWidth(800);
 		text.setStyle("-fx-font-size: 20px;");
+		vbox.setMaxWidth(1100);
 		vbox.getChildren().addAll(text);
 
+		itemGroup = new ToggleGroup();
+		existingPlants = new ToggleButton("Existing Plants");
+		selectedPlants = new ToggleButton("Selected Plants");
+		obstacles = new ToggleButton("Obstacles");
 
+		existingPlants.setToggleGroup(itemGroup);
+		selectedPlants.setToggleGroup(itemGroup);
+		obstacles.setToggleGroup(itemGroup);
+
+		optionHBox = new HBox(existingPlants, selectedPlants, obstacles);
+
+		existingPlants.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("ExistingPlants: selected");
+				/**
+				 * TODO: add a function here to add the correct tiles
+				 */
+			}
+		});
+
+		selectedPlants.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+
+				System.out.println("SelectedPlants: selected");
+				/**
+				 * TODO: add a function here to add the correct tiles
+				 */
+			}
+		});
+
+		obstacles.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+
+				System.out.println("Obstacles: selected");
+				/**
+				 * TODO: add a function here to add the correct tiles
+				 */
+			}
+		});
+
+		flow = new FlowPane();
+		flow.setPadding(new Insets(5, 5, 5, 5));
+		flow.setVgap(10);
+		flow.setHgap(10);
+		flow.setPrefWrapLength(270);
+		flow.setStyle("-fx-background-color: DAE6F3;");
+
+		// This is used for testing purposes
+		Button pages[] = new Button[30];
+		for (int i = 0; i < 30; i++) {
+			pages[i] = new Button("hi");
+			pages[i].setStyle("-fx-font-size: 32px;");
+			flow.getChildren().add(pages[i]);
+		}
+
+		scroll = new ScrollPane();
+		scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+		scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+		scroll.setVmax(440);
+		scroll.setPrefSize(850, 600);
+
+		scroll.setContent(flow);
+
+		animalsFedTxt = new Text("Animals Fed");
+		contBloomTxt = new Text("Continuous Bloom");
+		matchTxt = new Text("Matches Garden");
+		transitionTxt = new Text("Transition");
+
+		animalsFedTxt.setStyle("-fx-font-size: 20px;");
+		contBloomTxt.setStyle("-fx-font-size: 20px;");
+		matchTxt.setStyle("-fx-font-size: 20px;");
+		transitionTxt.setStyle("-fx-font-size: 20px;");
+
+		animalsFedBar = new Rectangle(200, 10);
+		contBloomBar = new Rectangle(200, 10);
+		matchBar = new Rectangle(200, 10);
+		transitionBar = new Rectangle(200, 10);
+
+		animalsFedBar.setStroke(Color.BLACK);
+		contBloomBar.setStroke(Color.BLACK);
+		matchBar.setStroke(Color.BLACK);
+		transitionBar.setStroke(Color.BLACK);
+
+		animalsFedBar.setFill(Color.WHITE);
+		contBloomBar.setFill(Color.WHITE);
+		matchBar.setFill(Color.WHITE);
+		transitionBar.setFill(Color.WHITE);
+
+		autoRateVBox = new VBox();
+		autoRateVBox.setPrefWidth(255);
+		autoRateVBox.setPrefHeight(550);
+		autoRateVBox.setPadding(new Insets(5, 5, 5, 5));
+
+		editPlotText = new Text("\nEdit Your Plot");
+		editPlotText.setStyle("-fx-font-size: 20px;");
+		editPlotButton = new Button("Edit Plot");
+		editPlotButton.setPadding(new Insets(10, 5, 10, 5));
+		editPlotButton.setStyle("-fx-font-size: 50px;");
+		autoRateVBox.getChildren().addAll(animalsFedTxt, animalsFedBar, contBloomTxt, contBloomBar, matchTxt, matchBar,
+				transitionTxt, transitionBar, editPlotText, editPlotButton);
+
+		box = new Rectangle(620, 550);
+		box.setStroke(Color.BLACK);
+		box.setFill(Color.WHITE);
+
+		leftDropdownVBox.setPrefWidth(255);
+		leftDropdownVBox.setPrefHeight(550);
+		leftDropdownVBox.getChildren().addAll(optionHBox, scroll);
+
+		createButtons();
+
+		tilePane.setAlignment(Pos.CENTER);
+		tilePane.setPadding(new Insets(5));
+		tilePane.setHgap(100);
+		tilePane.getChildren().addAll(backButton, saveButton, loadButton, nextButton);
+
+		borderPane.setBackground(new Background(new BackgroundFill(Color.LAVENDERBLUSH, null, null)));
+		borderPane.setPadding(new Insets(10, 10, 10, 10));
+		borderPane.setTop(vbox);
+		borderPane.setRight(autoRateVBox);
+		borderPane.setLeft(leftDropdownVBox);
+		borderPane.setBottom(tilePane);
+		borderPane.setCenter(box);
+
+		this.root = new Group();
+		root.getChildren().add(borderPane);
+		this.scene = new Scene(this.root, 1100, 650);
+	}
+
+	/**
+	 * Creates the buttons that will be added to the tilePane at the bottom of the
+	 * screen
+	 */
+	public void createButtons() {
 		backButton = new Button("Go Back");
 		backButton.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-            	switchToWindow(Windows.PlantSelection);
-            }
-        });
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Go Back: going to PlantSelection window");
+				switchToWindow(Windows.PlantSelection);
+			}
+		});
+
+		saveButton = new Button("Save");
+		saveButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Save: saving work");
+				// add saving function
+			}
+		});
+
+		loadButton = new Button("Load");
+		loadButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Load: loading garden");
+				// add loading function
+			}
+		});
 
 		nextButton = new Button("Next");
 		nextButton.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-            	switchToWindow(Windows.SeasonView);
-            }
-        });
-
-		borderPane.setTop(vbox);
-		borderPane.setLeft(backButton);
-		borderPane.setRight(nextButton);
-
-		this.root = new Group();
-		root.getChildren().add(borderPane);
-		this.scene = new Scene(this.root);
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Next: going to seasonView window");
+				switchToWindow(Windows.SeasonView);
+			}
+		});
 	}
 
 	@Override
