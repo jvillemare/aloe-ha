bb13a9f3611fb3cf901c247480d96ad0ac4939c9
Megha Patel <mpatel@udel.edu>
2020-04-24 22:45:49 -0400

    Minor adjustments made to Welcome Screen

--- GardenProject/src/main/java/udel/GardenProject/windows/Welcome.java
+++ GardenProject/src/main/java/udel/GardenProject/windows/Welcome.java
@@ -11,9 +11,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import udel.GardenProject.enums.Windows;
 import udel.GardenProject.garden.Model;
@@ -25,97 +24,64 @@ import udel.GardenProject.garden.Model;
  */
 public class Welcome extends Window {
 
-
 	private Group root;
 	private Scene scene;
+
+	/**
+	 * Buttons to move to corresponding windows
+	 */
 	Button startNewPlot;
 	Button loadSavedPlot;
 	Button tutorialButton;
-	
+
+	/**
+	 * Images are tied to the buttons
+	 */
 	Image imageSeed;
 	Image imagePlant;
 	Image imageTree;
-	
+
 	private BorderPane welcomeScreen;
+
+	/**
+	 * Used for placing the buttons neatly
+	 */
 	private FlowPane buttonFlow;
-	
+
+	private Text welcome;
+
+	/**
+	 * Used for placing the title and the buttons in the center
+	 */
+	private VBox centerVBox;
+
 	public Welcome(Model m) {
 		super(m, "Welcome Menu");
-		
+
 		welcomeScreen = new BorderPane();
-		
-		Text welcome = new Text("Aloe-Ha to your Garden!");
-		welcome.setFont(new Font(25));
-		welcome.setFill(Color.DARKGREEN); 
-		
-		StackPane welcomeText = new StackPane();
-		welcomeText.getChildren().add(welcome);
-		
-		welcomeScreen.setStyle("-fx-background-color: DAE6F3;");
-		welcomeScreen.setTop(welcomeText);
-		
-		
+		welcome = new Text("Aloe-Ha to your Garden!");
+		welcome.setStyle("-fx-font-size: 50px;");
+		welcome.setFill(Color.DARKGREEN);
+
+		createButtons();
+
 		buttonFlow = new FlowPane(Orientation.HORIZONTAL);
 		buttonFlow.setPadding(new Insets(5, 0, 5, 0));
 		buttonFlow.setVgap(4);
 		buttonFlow.setHgap(4);
 		buttonFlow.setPrefWrapLength(600);
-		buttonFlow.setStyle("-fx-background-color: DAE6F3;");
-		
-		
-		imageSeed = new Image(
-				getClass().getResourceAsStream("/buttonImages/seed.png"),
-				300, 100, true, true);
-		startNewPlot = new Button("Start New Plot", new ImageView(imageSeed));
-		startNewPlot.setMaxSize(300,100);
-		
-		startNewPlot.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-            	switchToWindow(Windows.ExistingPlants);
-            }
-        });
-		
-		imagePlant = new Image(
-				getClass().getResourceAsStream("/buttonImages/images.png"),
-				300, 100, true, true);
-		
-		loadSavedPlot = new Button("Load Saved Plot", new ImageView(imagePlant));
-		loadSavedPlot.setMaxSize(300,100);
-		
-		loadSavedPlot.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-            	//switchToWindow(Windows.Questionnaire);
-            	//TODO: figure out how to handle this. 
-            }
-        });
-		
-		imageTree = new Image(
-				getClass().getResourceAsStream("/buttonImages/tree.png"),
-				300, 100, true, true);
-		tutorialButton = new Button("Tutorial", new ImageView(imageTree));
-		tutorialButton.setMaxSize(300, 100);
-		
-		tutorialButton.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-            	switchToWindow(Windows.Tutorial);
-            }
-        });
-		
-				
-		buttonFlow.getChildren().addAll(startNewPlot, loadSavedPlot,
-				tutorialButton);
-		
-		welcomeScreen.setCenter(buttonFlow);
-		
+		buttonFlow.getChildren().addAll(startNewPlot, loadSavedPlot, tutorialButton);
+
+		centerVBox = new VBox();
+		centerVBox.setPadding(new Insets(200, 200, 200, 200));
+		centerVBox.getChildren().addAll(welcome, buttonFlow);
+
+		welcomeScreen.setStyle("-fx-background-color: DAE6F3;");
+		welcomeScreen.setCenter(centerVBox);
+
 		this.root = new Group();
 		root.getChildren().add(welcomeScreen);
-		this.scene = new Scene(this.root);
+		this.scene = new Scene(this.root, 940, 580);
 	}
 
 	@Override
@@ -123,6 +89,43 @@ public class Welcome extends Window {
 		return this.scene;
 
 	}
-	
-	
+
+	public void createButtons() {
+
+		imageSeed = new Image(getClass().getResourceAsStream("/buttonImages/seed.png"), 300, 100, true, true);
+		startNewPlot = new Button("Start New Plot", new ImageView(imageSeed));
+		startNewPlot.setMaxSize(300, 100);
+		startNewPlot.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Start New Plot: switching to Existing Plants window");
+				switchToWindow(Windows.ExistingPlants);
+			}
+		});
+
+		imagePlant = new Image(getClass().getResourceAsStream("/buttonImages/images.png"), 300, 100, true, true);
+		loadSavedPlot = new Button("Load Saved Plot", new ImageView(imagePlant));
+		loadSavedPlot.setMaxSize(300, 100);
+		loadSavedPlot.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Load: loading saved plots");
+				// TODO: figure out how to handle this.
+			}
+		});
+
+		imageTree = new Image(getClass().getResourceAsStream("/buttonImages/tree.png"), 300, 100, true, true);
+		tutorialButton = new Button("Tutorial", new ImageView(imageTree));
+		tutorialButton.setMaxSize(300, 100);
+		tutorialButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Tutorial: switching to tutorial window");
+				switchToWindow(Windows.Tutorial);
+			}
+		});
+	}
 }
