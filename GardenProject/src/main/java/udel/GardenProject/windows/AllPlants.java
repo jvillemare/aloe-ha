98e99f19ae1561e6897593ace892a58166600a76
Viva Laas <vivalaas@udel.edu>
2020-04-26 12:57:21 -0400

    This commit has the updated PlantInfo and AllPlants such that it would allow for plant info to obtain information of what plant to display and what window to go back to (thinking either AllPlants or PlantSelection) AllPlants now has a search box that does not work yet

--- GardenProject/src/main/java/udel/GardenProject/windows/AllPlants.java
+++ GardenProject/src/main/java/udel/GardenProject/windows/AllPlants.java
@@ -4,6 +4,13 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import udel.GardenProject.garden.Model;
 import udel.GardenProject.plants.Plant;
 
@@ -19,34 +26,57 @@ public class AllPlants extends Window {
 	private Group root;
 	
 	/**
-	 * Array of Plants
-	 */
-	private Plant[] plants;
-	
-	/**
 	 * Button that saves ... TODO what?
 	 */
 	private Button saveButton;
-	
-	/**
-	 * Button that goes to the next window
-	 */
-	private Button nextButton;
 	
 	/**
 	 * Button that goes to the previous window
 	 */
 	private Button backButton;
 	
+	private BorderPane border;
+	
+	private HBox searchBox;
+	
 	public AllPlants(Model m) {
 		super(m, "Plant Database");
 		
 		Label label = new Label("So many plants...");
 		
+		border = new BorderPane();
+		
+		//left side will be filter
+		TextField text = new TextField();
+	
+		searchBox = new HBox();
+		
+		Button close = new Button("X");
+        Button search = new Button("Search");
+        searchBox.getChildren().addAll(text,close,search);
+		
+		Text title = new Text("Please Make Plant Selections");
+		title.setFont(new Font(18));
+		title.setFill(Color.DARKGREEN);
+		
+		StackPane txt = new StackPane();
+		txt.setStyle("-fx-background-color: DAE6F3;");
+		txt.getChildren().add(title);
+		
+		border.setTop(txt);
+		border.setStyle("-fx-background-color: DAE6F3;");
+		border.setLeft(searchBox);
+		
+		
+		
+		//center will be all plants
+		
+		
+		
 		//brings you too outside database
 		
 		this.root = new Group();
-		root.getChildren().add(label);
+		root.getChildren().add(border);
 		this.scene = new Scene(this.root, 200, 200);
 	}
 	
--- GardenProject/src/main/java/udel/GardenProject/windows/PlantInfo.java
+++ GardenProject/src/main/java/udel/GardenProject/windows/PlantInfo.java
@@ -2,8 +2,17 @@ package udel.GardenProject.windows;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import udel.GardenProject.enums.Windows;
 import udel.GardenProject.garden.Model;
 import udel.GardenProject.plants.Plant;
@@ -16,17 +25,18 @@ import udel.GardenProject.plants.Plant;
 public class PlantInfo extends Window {
 	
 	private Scene scene;
+	private Group root;
 	
-	private Plant plantArray[];
 	private Button backButton;
+	private Image plantImage;
+	
+	private BorderPane borderPane;
 
 	public PlantInfo(Model m) {
-		super(m, "Plant Info: "); 
-		// this will have to change to the name of the plant they've clicked on
+		super(m, "Plant Info: "); 		
 		
+		borderPane = new BorderPane();
 		
-		
-		// pop up window
 		
 		backButton = new Button("Go Back");
 		backButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -36,6 +46,9 @@ public class PlantInfo extends Window {
             	switchToWindow(Windows.Questionnaire);
             }
         });
+		
+		borderPane.setBottom(backButton);
+		
 		
 		
 	}
@@ -48,11 +61,57 @@ public class PlantInfo extends Window {
 	
 	/**
 	 * Change PlantInfo's scene and display a plant's info by it's latin name.
+	 * This will be used by PlantSelection and AllPlants for their button
+	 * getModel().getWindow(Windows.PlantInfo).displayPlant(myCoolPlant, Windows.AllPlants)
 	 * 
 	 * @param plantLatinName Linnaeus Genus species plant name.
 	 */
-	public void displayPlant(String plantLatinName) {
+	public void displayPlant(Plant plant, Windows previousWindow) {
 		// TODO: Keep String or use different param?
+		
+		setTitle("Plant Info: " + plant.getLatinName());
+		
+		Text name = new Text(plant.getLatinName());
+		name.setFont(new Font(20));
+		name.setFill(Color.DARKGREEN);
+		
+		StackPane namePane = new StackPane();
+		namePane.getChildren().add(name);
+		namePane.setStyle("-fx-background-color: DAE6F3;");
+		
+		Text light = new Text("Light: " + plant.getLight());
+		Text bloom = new Text("Bloom Season: " + plant.getBloomTime());
+		Text description = new Text (plant.getDescription());
+		Text info = new Text(bloom + "\n"
+				+ light + "\n" + description);
+		info.setFont(new Font(18));
+		info.setFill(Color.DARKGREEN);
+		
+		ImageView img = new ImageView();
+		plantImage = new Image(getClass().getResourceAsStream("/buttonImages/seed.png"),
+				300, 100, true, true);
+		img.setImage(plantImage);
+		
+		borderPane.setLeft(img);
+		borderPane.setCenter(info);
+		borderPane.setTop(namePane);
+		borderPane.setStyle("-fx-background-color: DAE6F3;");
+		
+		backButton = new Button("Go Back");
+		backButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+            	switchToWindow(previousWindow);
+            }
+        });
+		
+		borderPane.setBottom(backButton);
+		
+		this.root = new Group();
+		root.getChildren().add(borderPane);
+		this.scene = new Scene(this.root);
+		
 	}
 	
 }
