5d2ab29a0e76ecf16e56dff2ec0ef005f39a95a7
Megha Patel <mpatel@udel.edu>
2020-04-26 11:56:35 -0400

    Getting rid of arraylist made to questionnaire

--- GardenProject/src/main/java/udel/GardenProject/windows/Questionnaire.java
+++ GardenProject/src/main/java/udel/GardenProject/windows/Questionnaire.java
@@ -1,10 +1,6 @@
 package udel.GardenProject.windows;
 
 import javafx.collections.FXCollections;
-import java.util.ArrayList;
-
-import javax.script.Bindings;
-
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -22,6 +18,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.CheckBoxListCell;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
@@ -108,6 +105,9 @@ public class Questionnaire extends Window {
 				"Welcome to the Aloe-ha questionnaire! Please fill out the questions below. Remember, you must answer all of the questions to continue.\n");
 		text.setWrappingWidth(800);
 		text.setStyle("-fx-font-size: 20px;");
+		vbox.setStyle("-fx-background-color: DAE6F3;");
+		vbox.setPadding(new Insets(10, 10, 10, 10));
+		vbox.getChildren().add(text);
 
 		populateQuestionnaire();
 		createButtons();
@@ -120,78 +120,62 @@ public class Questionnaire extends Window {
 		scroll = new ScrollPane();
 		scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
 		scroll.setVmax(440);
-		scroll.setPrefSize(850, 600);
+		scroll.setPrefSize(830, 600);
 		scroll.setContent(vbox);
 
+		borderPane.setStyle("-fx-background-color: DAE6F3;");
 		borderPane.setRight(scroll);
 		borderPane.setTop(vbox);
 		borderPane.setBottom(tilePane);
 
 		this.root = new Group();
 		root.getChildren().add(borderPane);
-		this.scene = new Scene(this.root, 850, 650);
+		this.scene = new Scene(this.root, 830, 635);
 	}
 
 	/**
-	 * Pulls together the questions and how it is handled into the vbox
+	 * Pulls together the questions and how it is handled into the vbox. Ability to
+	 * change order of questions here.
 	 */
 	public void populateQuestionnaire() {
-		String[] questions = { "1) What would you like to name your Garden?",
-				"2) How big is the plot you wish to plant your garden",
-				"3) Is your plot near any of the following? (Please select all that apply)",
-				"4) Do you have any of the following items in your garden? (Please select all that apply)",
-				"5) Does your entire plot have the same level of moisture? If yes, what level of moisture does your garden have?",
-				"6) Does your entire plot have the same soil type? If yes, what soil type does your garden have?",
-				"7) Does your entire plot receive the same amount of sunlight? If yes, to what degree of lighing does your garden get?",
-				"8) When would you like to see your garden bloom? (Please select all that apply?",
-				"9) What color blooms would you like to see in your garden? (Please select all that apply)" };
-
-		q0 = new Text(questions[0]);
 		createQ1();
-
-		q1 = new Text(questions[1]);
 		createQ2();
-
-		q2 = new Text(questions[2]);
 		createQ3();
-
-		q3 = new Text(questions[3]);
 		createQ4();
-
-		q4 = new Text(questions[4]);
 		createQ5();
-
-		q5 = new Text(questions[5]);
 		createQ6();
-
-		q6 = new Text(questions[6]);
 		createQ7();
-
-		q7 = new Text(questions[7]);
 		createQ8();
-
-		q8 = new Text(questions[8]);
 		createQ9();
+	}
 
-		vbox.setPadding(new Insets(10, 10, 10, 10));
-		vbox.getChildren().addAll(text, q0, hbname, q1, hbwidth, hbheight, q2, q2ListView, q3, q3ListView, q4,
-				q4ChoiceBox, q5, q5ChoiceBox, q6, q6ChoiceBox, q7, q7ListView, q8, q8ListView);
+	/**
+	 * Creates the Text of each question
+	 * 
+	 * @param question
+	 * @return
+	 */
+	public Text createText(String question) {
+		Text t = new Text(question);
+		t.setWrappingWidth(800);
+		t.setStyle("-fx-font-size: 20px;");
+		return t;
 	}
 
 	public void createQ1() {
-		q0.setWrappingWidth(800);
-		q0.setStyle("-fx-font-size: 20px;");
+		q0 = createText("1) What would you like to name your Garden?");
 
 		gardenLabel = new Label("Garden Name:");
 		textField = new TextField();
 		hbname = new HBox();
 		hbname.getChildren().addAll(gardenLabel, textField);
 		hbname.setSpacing(10);
+
+		vbox.getChildren().addAll(q0, hbname);
 	}
 
 	public void createQ2() {
-		q1.setWrappingWidth(800);
-		q1.setStyle("-fx-font-size: 20px;");
+		q1 = createText("2) How big is the plot you wish to plant your garden");
 
 		gardenWidth = new Label("Width:");
 		q1textField1 = new TextField();
@@ -204,11 +188,12 @@ public class Questionnaire extends Window {
 		hbheight = new HBox();
 		hbheight.getChildren().addAll(gardenHeight, q1textField2);
 		hbheight.setSpacing(10);
+
+		vbox.getChildren().addAll(q1, hbwidth, hbheight);
 	}
 
 	public void createQ3() {
-		q2.setWrappingWidth(800);
-		q2.setStyle("-fx-font-size: 20px;");
+		q2 = createText("3) Is your plot near any of the following? (Please select all that apply)");
 
 		q2checkBox1 = new CheckBox("Road");
 		q2checkBox2 = new CheckBox("Forest");
@@ -217,11 +202,13 @@ public class Questionnaire extends Window {
 		ObservableList<CheckBox> q1items = FXCollections.observableArrayList(q2checkBox1, q2checkBox2);
 		q2ListView.setItems(q1items);
 		q2ListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+		vbox.getChildren().addAll(q2, q2ListView);
 	}
 
 	public void createQ4() {
-		q3.setWrappingWidth(800);
-		q3.setStyle("-fx-font-size: 20px;");
+
+		q3 = createText("4) Do you have any of the following items in your garden? (Please select all that apply)");
 
 		q3checkBox1 = new CheckBox("Fence");
 		q3checkBox2 = new CheckBox("Pool");
@@ -239,37 +226,45 @@ public class Questionnaire extends Window {
 				q3checkBox4, q3checkBox5, q3checkBox6, q3checkBox7, q3checkBox8, q3checkBox9, q3checkBox10);
 		q3ListView.setItems(q2items);
 		q3ListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+		vbox.getChildren().addAll(q3, q3ListView);
 	}
 
 	public void createQ5() {
-		q4.setWrappingWidth(800);
-		q4.setStyle("-fx-font-size: 20px;");
+		q4 = createText(
+				"5) Does your entire plot have the same level of moisture? If yes, what level of moisture does your garden have?");
 
 		q4ChoiceBox = new ChoiceBox<>();
 		q4ChoiceBox.getItems().addAll("Dry", "Dry-moist", "Moist", "Moist-damp", "Damp",
 				"My plot has different moisture");
+
+		vbox.getChildren().addAll(q4, q4ChoiceBox);
 	}
 
 	public void createQ6() {
-		q5.setWrappingWidth(800);
-		q5.setStyle("-fx-font-size: 20px;");
+		q5 = createText(
+				"6) Does your entire plot have the same soil type? If yes, what soil type does your garden have?");
 
 		q5ChoiceBox = new ChoiceBox<>();
 		q5ChoiceBox.getItems().addAll("Clay", "Sandy", "Loamy", "My plot has different soil types");
+
+		vbox.getChildren().addAll(q5, q5ChoiceBox);
 	}
 
 	public void createQ7() {
-		q6.setWrappingWidth(800);
-		q6.setStyle("-fx-font-size: 20px;");
+		q6 = createText(
+				"7) Does your entire plot receive the same amount of sunlight? If yes, to what degree of lighing does your garden get?");
 
 		q6ChoiceBox = new ChoiceBox<>();
 		q6ChoiceBox.getItems().addAll("Full-sun", "Partial-shade", "Partial-sun", "Full-shade",
 				"My plot receives different levels of sunlight");
+
+		vbox.getChildren().addAll(q6, q6ChoiceBox);
 	}
 
 	public void createQ8() {
-		q7.setWrappingWidth(800);
-		q7.setStyle("-fx-font-size: 20px;");
+
+		q7 = createText("8) When would you like to see your garden bloom? (Please select all that apply?");
 
 		q7checkBox1 = new CheckBox("Spring");
 		q7checkBox2 = new CheckBox("Summer");
@@ -282,11 +277,13 @@ public class Questionnaire extends Window {
 				q7checkBox4, q7checkBox5);
 		q7ListView.setItems(q7items);
 		q7ListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+		vbox.getChildren().addAll(q7, q7ListView);
 	}
 
 	public void createQ9() {
-		q8.setWrappingWidth(800);
-		q8.setStyle("-fx-font-size: 20px;");
+
+		q8 = createText("9) What color blooms would you like to see in your garden? (Please select all that apply)");
 
 		q8checkBox1 = new CheckBox("White");
 		q8checkBox2 = new CheckBox("Yellow");
@@ -302,6 +299,7 @@ public class Questionnaire extends Window {
 				q8checkBox4, q8checkBox5, q8checkBox6, q8checkBox7, q8checkBox8);
 		q8ListView.setItems(q8items);
 		q8ListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		vbox.getChildren().addAll(q8, q8ListView);
 	}
 
 	public void createButtons() {
@@ -319,7 +317,8 @@ public class Questionnaire extends Window {
 			@Override
 			public void handle(ActionEvent event) {
 				System.out.println("Save: saving questionnaire responses");
-				// getResponses(...);
+				getResponses();
+
 			}
 		});
 
@@ -340,13 +339,11 @@ public class Questionnaire extends Window {
 
 	}
 
-	/*
-	 * TODO: change function so it gets all the answers at the same time This
-	 * function will be used in save.setOnAction
+	/**
+	 * TODO:
 	 */
-	private void getResponses(ChoiceBox<String> choiceBox) {
-		String choice = choiceBox.getValue();
-		System.out.println(choice);
+	private void getResponses() {
+
 	}
 
 }
