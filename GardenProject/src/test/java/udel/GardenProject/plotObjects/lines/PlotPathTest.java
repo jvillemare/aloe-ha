2899d6c49f580993ecabd49f5d56d1c6e8eec3d7
James Villemarette <jvill@udel.edu>
2020-05-15 10:36:35 -0400

    #173: fixed unit tests Model constructor

--- GardenProject/src/test/java/udel/GardenProject/plotObjects/lines/PlotPathTest.java
+++ GardenProject/src/test/java/udel/GardenProject/plotObjects/lines/PlotPathTest.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import udel.GardenProject.garden.Controller;
+import udel.GardenProject.garden.Model;
+
 public class PlotPathTest {
 
 	@Test
 	public void testPlotPath() {
-		PlotPath p=new PlotPath(0, 0);
+		Controller c = new Controller();
+		Model m = new Model(c, 0, 1);
+		PlotPath p = new PlotPath(m, 0, 0);
 		assertTrue(p.getWindowImage().equals("/viewImages/path.png"));
 		assertTrue(p.getWindowImage().equals("/viewImages/plotPath.png"));
 		assertTrue(p.getPlotX()==0);
--- GardenProject/src/test/java/udel/GardenProject/windows/TutorialTest.java
+++ GardenProject/src/test/java/udel/GardenProject/windows/TutorialTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import udel.GardenProject.garden.Controller;
 import udel.GardenProject.garden.Model;
 import udel.GardenProject.windows.Tutorial;
 
@@ -11,7 +12,8 @@ public class TutorialTest {
 
 	@Test
 	public void testTutorial() {
-		Model m = new Model(0, 1);
+		Controller c = new Controller();
+		Model m = new Model(c, 0, 1);
 		Tutorial t = new Tutorial(m);
 		assertTrue(t.getTitle().equals("Tutorial Window"));
 	}
